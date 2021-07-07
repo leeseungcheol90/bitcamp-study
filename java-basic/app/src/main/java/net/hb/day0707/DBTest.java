c52efad28adb841b4c2dc1ac2f0fa1527263b73e
leeseungcheol90 <zzori24@daum.net>
2021-07-07 17:19:17 +0900

    0707

---
+++ java-basic/app/src/main/java/net/hb/day0707/DBTest.java
@@ -0,0 +1,16 @@
+package net.hb.day0707;
+
+import java.sql.*;
+import java.util.*;
+
+
+public class DBTest {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Connection cn=null; // DB 서버 연결 정보
+    Statement st=null; // 명령어 생성
+    ResultSet rs=null; // 조회 결과를 기억
+  }
+
+}
